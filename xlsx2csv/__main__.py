576f4eb061f2f5a0a2601b085e5e0a13760fa236
Tom Dalby <thomasjackdalby@gmail.com>
2020-03-06 18:09:10 +0000

    updated to use argparse

--- xlsx2csv/__main__.py
+++ xlsx2csv/__main__.py
@@ -1,22 +1,30 @@
 # __main__.py
-# list the sheets 
 
-import sys, openpyxl, csv
-import os
+import sys, openpyxl, csv, os, argparse
 
-if len(sys.argv) < 2:
-    print("Need to provide a file-path to the xlsx file.")
-    exit()
+parser = argparse.ArgumentParser(prog="xlsx2csv")
+parser.add_argument("file_path", help="file-path of xlsx file to convert.")
+parser.add_argument("-s", "--sheets", nargs='*', default=None, help="names of worksheets to convert.")
 
-file_path = sys.argv[1]
+args = parser.parse_args(sys.argv[1:])
+
+file_path = args.file_path
 if not os.path.exists(file_path):
-    print(f"No file at [{file_path}]")
+    print(f"No file located at [{file_path}].")
     exit()
 
-print(f"Converting [{file_path}] to csv.")
 workbook = openpyxl.load_workbook(file_path)
-for worksheet in workbook:
-    csv_file_path = f"{worksheet.title}.csv" 
+
+if args.sheets:
+    worksheet_names = args.sheets
+else:
+    worksheet_names = workbook.sheetnames
+
+print(f"Converting [{file_path}] to csv...")
+for worksheet_name in worksheet_names:
+    worksheet = workbook[worksheet_name]
+    csv_file_path = f"{worksheet.title}.csv"
+    print(f"Exporting sheet [{worksheet_name}] to [{csv_file_path}].")
     with open(csv_file_path, "w", newline="") as file:
         writer = csv.writer(file)
         for row in worksheet.rows:
